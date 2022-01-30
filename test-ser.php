96432285d54556af650ec03c60694806547d19c3
John9Smith <97229261+John9Smith@users.noreply.github.com>
2022-01-30 10:26:02 +0200

    Add files via upload

---
+++ test-ser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Test Ser
+ * @version 1.1.0
+ * @author Asdamsoft
+ * @copyright 2021 Asdamsoft
+ * Plugin Name: Test Ser
+ * Plugin URI: https://www.asdamsoft.com/
+ * Description: Support Test Ser For Contact Form 7.
+ * Author: Asdamsoft
+ * Version: 1.1.0
+ * Author URI: https://www.asdamsoft.com/
+ * Text Domain: test-ser
+ * Domain Path: /languages
+*/
